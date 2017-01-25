3ef81ebedfe62f52f47f93faed7464b1bf746898
Sam Pastoriza <pastorsj@rose-hulman.edu>
2017-01-25 17:25:26 -0500

    Made python script a cron job and added commands to move script from local fs to hdfs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import thread
+import schedule
 import time
 import os
 import shutil
@@ -7,29 +8,40 @@ import ftse
 import dow
 import sse
 import snp
+from subprocess import call
 
-# Create four threads that independently run the scrapers
-try:
-    current_dir = os.getcwd()
-    ftse_dir = current_dir + '/' + ftse.get_index()
-    dow_dir = current_dir + '/' + dow.get_index()
-    sse_dir = current_dir + '/' + sse.get_index()
-    snp_dir = current_dir + '/' + snp.get_index()
-    if os.path.exists(ftse_dir):
-        shutil.rmtree(ftse_dir)
-    os.makedirs(ftse_dir)
-    if os.path.exists(dow_dir):
-        shutil.rmtree(dow_dir)
-    os.makedirs(dow_dir)
-    if os.path.exists(sse_dir):
-        shutil.rmtree(sse_dir)
-    os.makedirs(sse_dir)
-    if os.path.exists(snp_dir):
-        shutil.rmtree(snp_dir)
-    os.makedirs(snp_dir)
-    ftse.get_ftse()
-    dow.get_dow()
-    sse.get_sse()
-    snp.get_snp()
-except:
-    print "Error while getting data from Yahoo finance api"
+def job():
+    try:
+        current_dir = os.getcwd()
+        ftse_dir = current_dir + '/' + ftse.get_index()
+        dow_dir = current_dir + '/' + dow.get_index()
+        sse_dir = current_dir + '/' + sse.get_index()
+        snp_dir = current_dir + '/' + snp.get_index()
+        if os.path.exists(ftse_dir):
+            shutil.rmtree(ftse_dir)
+        os.makedirs(ftse_dir)
+        if os.path.exists(dow_dir):
+            shutil.rmtree(dow_dir)
+        os.makedirs(dow_dir)
+        if os.path.exists(sse_dir):
+            shutil.rmtree(sse_dir)
+        os.makedirs(sse_dir)
+        if os.path.exists(snp_dir):
+            shutil.rmtree(snp_dir)
+        os.makedirs(snp_dir)
+        ftse.get_ftse()
+        dow.get_dow()
+        sse.get_sse()
+        snp.get_snp()
+        call("hadoop fs -put dow/ /tmp/StockData/dow/", shell=True)
+        call("hadoop fs -put ftse/ /tmp/StockData/ftse/", shell=True)
+        call("hadoop fs -put sse/ /tmp/StockData/sse/", shell=True)
+        call("hadoop fs -put snp/ /tmp/StockData/snp/", shell=True)
+    except:
+        print "Error while getting data from Yahoo finance api"
+
+schedule.every().day.at("4:00").do(job)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
